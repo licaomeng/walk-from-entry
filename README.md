a7bdfaa90ec28c369408d5b0a000697f68b1b0c8
Caomeng Li <caomengli@microsoft.com>
2021-04-29 22:27:53 +0800

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # walk-from-entry
-Walk through a monorepo to execute commands like `build` `test`, provides various modes such as `--fast`, `--full`, '--incremental, -inc'
+
+Walk through a monorepo to execute commands like `npm run build` `npm run test`, provides multiple modes such as `--fast`, `--full`, `--incremental, -inc`
+
+## Synopsis
+
+`npx walk-from-entry <command> [command-arg]...`
+
+- `<command>`: build, test, lint...
+- `[command-arg]`: --fast, --full, --incremental/-inc
+
+## Usage
+
+1. Switch to your entry directory.
+2. run the command `npx walk-from-entry build -inc`
+
+> The above command is equivalent to incrementally executing `npm run build` across all dependencies packages in the monorepo.
+
+## Description
+
+### < command >
+
+<command> can fill the slot `npm run <command>`
+
+```json
+"scripts": {
+  "build": "...",
+  "test": "...",
+  "lint": "...",
+},
+```
+
+### [command-arg]
+
+- `--fast` - only for no dist dependencies, if you execute `npx walk-from-entry build --fast`, it will just build the packages that have no dist folder.
+
+- `--full` - fully execute the command.
+- `-inc, --incremental` - incrementally execute the command. The mode defaults to be`--incremental`. ie, `npx walk-from-entry build` is equivalent to `npx walk-from-entry build --incremental` or `npx walk-from-entry build -inc`. The mechanism of the incremental build is generating a source code hash file `__hash.json` in dist folder. Once run it again, compare the previous one and the current one, to determine building it or skip.
+
+## License
+
+[MIT](https://github.com/licaomeng/walk-from-entry/blob/master/LICENSE)
---
+++ exec.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const cp = require('child_process');
+const path = require('path');
+const chalk = require('chalk');
+const entryDir = process.cwd();
+const hash = require('./hash');
+const tsconfigJSON = 'tsconfig.json';
+
+const modeMap = {
+  '--fast': 'fast',
+  '--full': 'full',
+  '--incremental': 'incremental',
+  '-inc': 'incremental',
+};
+
+function getDepsFromEntry(entryDir) {
+  const arr = [];
+  function traverse(currDir) {
+    // avoid overlap cases
+    if (arr.includes(currDir)) {
+      return arr;
+    }
+    const tsconfig = path.join(currDir, tsconfigJSON);
+    // base
+    if (!fs.existsSync(tsconfig)) {
+      return arr;
+    }
+    const obj = require(tsconfig);
+    const refs = obj.references;
+    const folders = refs
+      .reduce((ret, item) => [...ret, item.path], [])
+      .map(item => path.join(currDir, item));
+
+    // Postorder Traversal
+    folders.forEach(folder => {
+      traverse(folder);
+    });
+    arr.push(...folders);
+
+    return arr;
+  }
+  return traverse(entryDir);
+}
+
+function validateArgs(cmd, mode) {
+  if (!cmd) {
+    return chalk.red('The command cannot be empty!');
+  }
+  if (!modeMap[mode]) {
+    return `${chalk.red('The mode should be one of the following:')}\n
+    ${chalk.green('--fast, --full, --incremental/-inc')}`;
+  }
+}
+
+async function exec(...args) {
+  const [cmd, mode = '--incremental'] = args;
+  const errMsg = validateArgs(cmd, mode);
+  if (errMsg) {
+    console.log(errMsg);
+    return;
+  }
+
+  const deps = getDepsFromEntry(entryDir);
+  const dirs = Array.from(new Set(deps));
+
+  console.log(`start ${chalk.yellow(cmd)} from the entry path: ${chalk.blue(entryDir)}`);
+
+  for (let i = 0; i < dirs.length; i++) {
+    const dir = dirs[i];
+    const dist = path.join(dir, 'dist');
+    const src = path.join(dir, 'src');
+    const __hashJson = path.join(dist, '__hash.json');
+    const hashInfo = await hash(src);
+
+    if (mode === '--fast') {
+      if (fs.existsSync(dist)) {
+        continue;
+      }
+    } else if (['--incremental', '-inc'].includes(mode)) {
+      // no change
+      if (
+        fs.existsSync(dist) &&
+        fs.existsSync(__hashJson) &&
+        require(__hashJson).hash === hashInfo.hash
+      ) {
+        continue;
+      }
+    }
+    cp.execSync(`npm run ${cmd}`, {
+      cwd: dir,
+      stdio: 'inherit',
+    });
+    if (fs.existsSync(dist)) {
+      fs.writeFileSync(__hashJson, JSON.stringify(hashInfo));
+    }
+  }
+  console.log(chalk.green(`${modeMap[mode]} build done!`));
+}
+
+module.exports = exec;
